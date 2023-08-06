147f7016c03fc42e1bf28af4c4f54d1eb9ad3409
Juan David <juanemanueldavidzaracho@gmail.com>
2023-08-05 20:38:49 -0400

    Toast ok

--- app/src/main/java/com/example/pruebasandroid/MainActivity.kt
+++ app/src/main/java/com/example/pruebasandroid/MainActivity.kt
@@ -1,22 +1,26 @@
 package com.example.pruebasandroid
 
 import android.os.Bundle
+
+import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.pruebasandroid.ui.theme.PruebasAndroidTheme
+import com.example.pruebasandroid.databinding.ActivityMainBinding
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val name = binding.txtName.text
+        val nick = binding.txtNick.text
 
+        //usando lanbda
+        binding.btn.setOnClickListener{
+            Toast.makeText(this, "Hola $name $nick", Toast.LENGTH_LONG).show()
+        }
 
     }
 }
+
 
